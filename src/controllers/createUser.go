adfc9b4c02ecffafa23da256bc4b0b9ba51f26b1
LulianoM <martinslucianofigueira@gmail.com>
2022-04-21 17:05:13 -0300

    USER-003-Validate-Function-Username

--- src/controllers/createUser.go
+++ src/controllers/createUser.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"apiposterr/src/structs"
+	"errors"
+	"regexp"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,5 +19,22 @@ func CreateUser(c *fiber.Ctx) error {
 		Username: data["username"],
 	}
 
+	if err := validadeUsername(user); err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "invalid username",
+		})
+	}
+
 	return c.JSON(user)
+}
+
+func validadeUsername(user structs.User) error {
+	if len(user.Username) > 14 {
+		return errors.New("username bigger than 14 characters")
+	}
+	if regexp.MustCompile(`^[a-zA-Z0-9]*$`).MatchString(user.Username) != true {
+		return errors.New("username does not contain only alphanumeric characters")
+	}
+	return nil
 }
