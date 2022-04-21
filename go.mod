54abe70c97a23bd582fa6194d0f71224678209db
LulianoM <martinslucianofigueira@gmail.com>
2022-04-21 16:21:57 -0300

    USER-003-Struct-User

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module apiposterr
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.32.0
+require (
+	github.com/gofiber/fiber/v2 v2.32.0
+	github.com/google/uuid v1.3.0
+)
---
+++ src/structs/user.go
@@ -0,0 +1,15 @@
+package structs
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Username   string     `json:"username"`
+	DataJoined *time.Time `json:"data_joined"`
+	Followers  []string   `json:"followers"`
+	Following  []string   `json:"following"`
+	IdAccount  uuid.UUID
+}
