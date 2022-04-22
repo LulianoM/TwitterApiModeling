4961e4836795fd7430d8a8deeee4c939ee28a3ed
LulianoM <martinslucianofigueira@gmail.com>
2022-04-22 13:57:59 -0300

    USER-005-Add-New-Endpoints-GetAll-GetByID

---
+++ src/controllers/searchUser.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"apiposterr/src/database"
+	"apiposterr/src/structs"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetUser(c *fiber.Ctx) error {
+	var users []structs.User
+	database.DB.Find(&users)
+	return c.JSON(users)
+}
+
+func GetUserByID(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	var user structs.User
+
+	user.ID = uint(id)
+
+	database.DB.Find(&user)
+
+	return c.JSON(user)
+}
