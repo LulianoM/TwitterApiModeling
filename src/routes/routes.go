046b34846262f382e416bf32cf0cda5c11dca3df
LulianoM <martinslucianofigueira@gmail.com>
2022-04-26 22:06:54 -0300

    FIX-000-Refactor-Routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -10,14 +10,15 @@ import (
 
 func Setup(app *fiber.App) {
 	api := app.Group("api")
+	admin := api.Group("admin")
 
 	api.Get("ping", healtcheck.Ping)
 
 	api.Post("create", user.CreateUser)
-	api.Get("user", user.GetUser)
-	api.Get("user/:id", user.GetUserByID)
+	admin.Get("user", user.GetUser)
+	admin.Get("user/:id", user.GetUserByID)
 
 	api.Post("post", post.CreatePost)
-	api.Get("post", post.GetPosts)
-	api.Get("post/:id", post.GetPostByID)
+	admin.Get("post", post.GetPosts)
+	admin.Get("post/:id", post.GetPostByID)
 }
